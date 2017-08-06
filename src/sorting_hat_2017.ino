3aaa41f2f8796d89c06b8b35736a91545f1a03a7
steffeki <kim.steffenhagen@hotmail.com>
2017-08-06 16:32:51 -0500

    Updating sorting_hat_2017.ino
    Added touch sensor capability to the file.

--- src/sorting_hat_2017.ino
+++ src/sorting_hat_2017.ino
@@ -1,6 +1,9 @@
 // Add the servo library
 #include <Servo.h>
 
+// Define pin for touch sensor
+#define TOUCH_SENSOR 8
+
 // Define the servos
 Servo servo1;
 Servo servo2;
@@ -18,27 +21,36 @@ void setup() {
   servo3.attach(5);
   servo4.attach(6);
   servo5.attach(7);
+  
+  // Define touch sensor as input (Digital PWM 8)
+  pinMode(TOUCH_SENSOR, INPUT);
 }
 
 void loop() {
-  // Scan from 0 to 90 degrees
-  for (servoPos = 0; servoPos < 90; servoPos++) {
-    servo1.write(servoPos);
-    servo2.write(servoPos);
-    servo3.write(servoPos);
-    servo4.write(servoPos);
-    servo5.write(servoPos);
-    delay(10);
-  }
+  // Read the current value of the sensor
+  int sensorValue = digitalRead(TOUCH_SENSOR);
+  
+  // If the touch sensor is in contact with skin,
+  // activate the servos
+  if (!sensorValue) {
+    // Scan from 0 to 90 degrees
+    for (servoPos = 0; servoPos < 90; servoPos++) {
+      servo1.write(servoPos);
+      servo2.write(servoPos);
+      servo3.write(servoPos);
+      servo4.write(servoPos);
+      servo5.write(servoPos);
+      delay(10);
+    }
 
-  // Scan back from 90 to 0 degrees
-  for (servoPos = 90; servoPos > 0; servoPos--) {
-    servo1.write(servoPos);
-    servo2.write(servoPos);
-    servo3.write(servoPos);
-    servo4.write(servoPos);
-    servo5.write(servoPos);
-    delay(10);
+    // Scan back from 90 to 0 degrees
+    for (servoPos = 90; servoPos > 0; servoPos--) {
+      servo1.write(servoPos);
+      servo2.write(servoPos);
+      servo3.write(servoPos);
+      servo4.write(servoPos);
+      servo5.write(servoPos);
+      delay(10);
+    }
   }
 }
-
